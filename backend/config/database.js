5368f7083aa311540d559788ff035bdfd52207d2
muhammadareeb313 <areebmuhammad96@gmail.com>
2022-09-24 20:11:29 +0500

    final try

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose=require("mongoose");
 
-const connectDatabase = () => {
-  mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
-    .then((data) => {
-      console.log(`Mongodb connected with server: ${data.connection.host}`);
-    });
-};
-
-module.exports = connectDatabase;
+module.exports = mongoose.connect(process.env.DB_URI)
+.then((data)=>{
+  console.log(`Database Connect${data.connection.host}`)
+});
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const cloudinary = require("cloudinary");
-const connectDatabase = require("./config/database");
+// const connectDatabase = require("./config/database");
+const database = require("./config/database")
 
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
@@ -15,7 +16,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 // Connecting to database
-connectDatabase();
+// connectDatabase();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
